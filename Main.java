ae6a6b149d05056648f276bfbc6226da19986ff2
aydig8139 <aydig8139@wrdsb.ca>
2020-10-06 16:30:22 +0000

    unfinished question

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * print *s in the shape of a box
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for input and declare variable to store it 
+    System.out.println("Please enter a number between 1 and 10");
+    int userNum = input.nextInt();
+
+
+    for(int count = 1; count <= userNum; count++){
+      System.out.print("*");
+
+    }
+    
     
   }
 }
